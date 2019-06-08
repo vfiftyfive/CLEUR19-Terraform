9c9687060c3070d0532b41c56306c96b7176e5c5
vfiftyfive <vfiftyfive@gmail.com>
2019-06-08 16:50:27 -0700

    Update aci_test.tf

--- aci_test.tf
+++ aci_test.tf
@@ -2,7 +2,7 @@ provider "aci" {
   username    = "api_user"
   private_key = "${var.aci_private_key}"
   cert_name   = "${var.aci_cert_name}"
-  url         = "https://10.51.48.5"
+  url         = "${var.apic_url}"
   insecure    = true
 }
 
